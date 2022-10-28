53254f659ac18872580a248e9e0c8f4f47b25f68
AtakanAtabeyoglu <atakan.atabeyoglu@advanco.com>
2022-10-28 12:17:40 +0300

    permission screen is updated

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export default function Checkbox({header, checked, disabled}) {
+export default function Checkbox({onChange, header, checked, disabled}) {
   return (
     <div style={{flexDirection: 'row'}}>
-      <input style={{width: 20, height: 20, margin: 10}} class="form-check-input" type="checkbox" checked={checked} disabled={disabled}></input>
+      <input
+        onChange={onChange}
+        style={{width: 20, height: 20, margin: 10}}
+        class="form-check-input"
+        type="checkbox"
+        checked={checked}
+        disabled={disabled}></input>
       <label style={{marginTop: 10}} class="form-check-label">
         {header}
       </label>
--- src/pages/Permission/Permissions.js
+++ src/pages/Permission/Permissions.js
@@ -10,6 +10,7 @@ const Permissions = () => {
   const [data, setData] = useState(Mock);
   const [screenSize] = GetDynamicDimensions();
   const {dynamicHeight, dynamicWidth} = screenSize;
+  const [ force, setForce ] = useState(false) // TO FORCE THE RENDER AFTER USER PRESSED ON A CHECKBOX
 
   const container = {
     display: 'flex',
@@ -17,44 +18,62 @@ const Permissions = () => {
     flexDirection: 'column',
     height: dynamicHeight - dynamicHeight / 6,
     overflow: 'scroll',
-    padding: 0,
+    padding: 0
   };
   const mainDiv = {
     display: 'flex',
+    marginLeft: 5,
+    marginRight: 5
   };
-  const headerDiv = {backgroundColor: 'yellow', borderWidth: 1, borderStyle: 'solid', height: 40, minWidth: 250};
-  const permDiv = {display: 'flex'};
-  const columnsArray = {
-    backgroundColor: 'green',
+  const headerDiv = index => ({
+    backgroundColor: index % 2 === 0 ? '#FFFFFF' : '#BFBFBF',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    height: 40,
+    minWidth: 250
+  });
+  const permDiv = {
+    display: 'flex'
+  };
+  const columnsArray = (index = 0) => ({
+    backgroundColor: index % 2 === 0 ? '#FFFFFF' : '#BFBFBF',
     width: 70,
     borderWidth: 1,
     height: 40,
     borderStyle: 'solid',
     textAlign: 'center',
-  };
+  });
 
+  const handleChange = (title, objIdx) => {
+    const checkboxValue = data[objIdx][title];
+    data[objIdx][title] = checkboxValue === 0 ? 1 : 0;
+    setData(data);
+    setForce(p => !p)
+    return;
+  }
+  
   return (
     <Box sx={container}>
       <div style={mainDiv}>
-        <div style={headerDiv}>{'ControlId'}</div>
+        <div style={headerDiv(1)}>{'ControlId'}</div>
         <div style={permDiv}>
           {columns.map(item =>
-            item == 'formName' ? <div style={{...columnsArray, minWidth: 120}}>{item}</div> : <div style={columnsArray}>{item}</div>,
+            item == 'formName' ? <div style={{...columnsArray(1), minWidth: 120}}>{item}</div> : <div style={columnsArray(1)}>{item}</div>,
           )}
         </div>
       </div>
-      {data.map(a => {
+      {data.length > 0 && data.map((a, index) => {
         return (
           <div style={mainDiv}>
-            <div style={headerDiv}>{a.controlId}</div>
+            <div style={headerDiv(index)}>{a.controlId}</div>
             <div style={permDiv}>
               {columns.map(item => (
                 <>
                   {item == 'formName' ? (
-                    <div style={{...columnsArray, minWidth: 120}}>{a[item]}</div>
+                    <div style={{...columnsArray(index), minWidth: 120}}>{a[item]}</div>
                   ) : (
-                    <div style={columnsArray}>
-                      <CheckBox checked={a[item] == 1 ? true : a[item] == 9 ? true : false} disabled={a[item] == 9 ? true : false} />
+                    <div style={columnsArray(index)}>
+                      <CheckBox onChange={() => handleChange(item, index)} checked={a[item] == 1 ? true : a[item] == 9 ? true : false} disabled={a[item] == 9 ? true : false} />
                     </div>
                   )}
                 </>
